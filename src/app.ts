aac35cd38859cc886326c41cbb26b1ba27e97ede
IbragimSay <121868859+IbragimSay@users.noreply.github.com>
2024-03-17 13:19:14 +0300

    feat: Добавил роутер login Добавил роутер /api/auth/login

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, {Express} from 'express'
+import router from './router'
 
 const app:Express = express()
 
-app.get('/', (req, res)=>{
-    res.send('<h1>argen</h1>')
-})
+app.use("/api", router)
 
 export default app
