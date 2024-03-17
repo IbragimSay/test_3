881c762178d885ac14765f1b6ce0152d5b935890
IbragimSay <121868859+IbragimSay@users.noreply.github.com>
2024-03-17 13:31:24 +0300

    feat: Добавил роутер logup Добавил роутер /api/auth/logup

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 
 const router = Router()
 
+router.post('/logup', (req, res)=>{
+    res.send('auth router')
+})
 router.post('/login', (req, res)=>{
     res.send('auth router')
 })
